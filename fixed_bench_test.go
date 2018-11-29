f4d1eb469f916539aa514571e6a2a44b75719451
robaho <rengels@ix.netcom.com>
2018-11-29 00:35:19 -0600

    Initial commit

---
+++ fixed_bench_test.go
@@ -0,0 +1,173 @@
+package fixed
+
+import (
+	"github.com/shopspring/decimal"
+	"math/big"
+	"testing"
+)
+
+func BenchmarkAddFixed(b *testing.B) {
+	f0 := NewF(1)
+	f1 := NewF(1)
+
+	for i := 0; i < b.N; i++ {
+		f1 = f1.Add(f0)
+	}
+}
+func BenchmarkAddDecimal(b *testing.B) {
+	f0 := decimal.NewFromFloat(1)
+	f1 := decimal.NewFromFloat(1)
+
+	for i := 0; i < b.N; i++ {
+		f1 = f1.Add(f0)
+	}
+}
+func BenchmarkAddBigInt(b *testing.B) {
+	f0 := big.NewInt(1)
+	f1 := big.NewInt(1)
+
+	for i := 0; i < b.N; i++ {
+		f1 = f1.Add(f1, f0)
+	}
+}
+func BenchmarkAddBigFloat(b *testing.B) {
+	f0 := big.NewFloat(1)
+	f1 := big.NewFloat(1)
+
+	for i := 0; i < b.N; i++ {
+		f1 = f1.Add(f1, f0)
+	}
+}
+
+func BenchmarkMulFixed(b *testing.B) {
+	f0 := NewF(123456789.0)
+	f1 := NewF(1234.0)
+
+	for i := 0; i < b.N; i++ {
+		f0.Mul(f1)
+	}
+}
+func BenchmarkMulDecimal(b *testing.B) {
+	f0 := decimal.NewFromFloat(123456789.0)
+	f1 := decimal.NewFromFloat(1234.0)
+
+	for i := 0; i < b.N; i++ {
+		f0.Mul(f1)
+	}
+}
+func BenchmarkMulBigInt(b *testing.B) {
+	f0 := big.NewInt(123456789)
+	f1 := big.NewInt(1234)
+
+	var x big.Int
+	for i := 0; i < b.N; i++ {
+		x.Mul(f0, f1)
+	}
+}
+func BenchmarkMulBigFloat(b *testing.B) {
+	f0 := big.NewFloat(123456789.0)
+	f1 := big.NewFloat(1234.0)
+
+	var x big.Float
+	for i := 0; i < b.N; i++ {
+		x.Mul(f0, f1)
+	}
+}
+
+func BenchmarkDivFixed(b *testing.B) {
+	f0 := NewF(123456789.0)
+	f1 := NewF(1234.0)
+
+	for i := 0; i < b.N; i++ {
+		f0.Div(f1)
+	}
+}
+func BenchmarkDivDecimal(b *testing.B) {
+	f0 := decimal.NewFromFloat(123456789.0)
+	f1 := decimal.NewFromFloat(1234.0)
+
+	for i := 0; i < b.N; i++ {
+		f0.Div(f1)
+	}
+}
+func BenchmarkDivBigInt(b *testing.B) {
+	f0 := big.NewInt(123456789)
+	f1 := big.NewInt(1234)
+
+	var x big.Int
+	for i := 0; i < b.N; i++ {
+		x.Div(f0, f1)
+	}
+}
+func BenchmarkDivBigFloat(b *testing.B) {
+	f0 := big.NewFloat(123456789.0)
+	f1 := big.NewFloat(1234.0)
+
+	var x big.Float
+	for i := 0; i < b.N; i++ {
+		x.Quo(f0, f1)
+	}
+}
+
+func BenchmarkCmpFixed(b *testing.B) {
+	f0 := NewF(123456789.0)
+	f1 := NewF(1234.0)
+
+	for i := 0; i < b.N; i++ {
+		f0.Cmp(f1)
+	}
+}
+func BenchmarkCmpDecimal(b *testing.B) {
+	f0 := decimal.NewFromFloat(123456789.0)
+	f1 := decimal.NewFromFloat(1234.0)
+
+	for i := 0; i < b.N; i++ {
+		f0.Cmp(f1)
+	}
+}
+func BenchmarkCmpBigInt(b *testing.B) {
+	f0 := big.NewInt(123456789)
+	f1 := big.NewInt(1234)
+
+	for i := 0; i < b.N; i++ {
+		f0.Cmp(f1)
+	}
+}
+func BenchmarkCmpBigFloat(b *testing.B) {
+	f0 := big.NewFloat(123456789.0)
+	f1 := big.NewFloat(1234.0)
+
+	for i := 0; i < b.N; i++ {
+		f0.Cmp(f1)
+	}
+}
+
+func BenchmarkStringFixed(b *testing.B) {
+	f0 := NewF(123456789.12345)
+
+	for i := 0; i < b.N; i++ {
+		f0.String()
+	}
+}
+func BenchmarkStringDecimal(b *testing.B) {
+	f0 := decimal.NewFromFloat(123456789.12345)
+
+	for i := 0; i < b.N; i++ {
+		f0.String()
+	}
+}
+func BenchmarkStringBigInt(b *testing.B) {
+	f0 := big.NewInt(123456789)
+
+	for i := 0; i < b.N; i++ {
+		f0.String()
+	}
+}
+func BenchmarkStringBigFloat(b *testing.B) {
+	f0 := big.NewFloat(123456789.12345)
+
+	for i := 0; i < b.N; i++ {
+		f0.String()
+	}
+}
+
---
+++ fixed_test.go
@@ -0,0 +1,320 @@
+package fixed_test
+
+import (
+	. "github.com/robaho/fixed"
+	"math"
+	"testing"
+)
+
+func TestBasic(t *testing.T) {
+	f0 := NewS("123.456")
+	f1 := NewS("123.456")
+
+	if !f0.Equal(f1) {
+		t.Error("should be equal", f0, f1)
+	}
+
+	if f0.Int() != 123 {
+		t.Error("should be equal", f0.Int(), 123)
+	}
+
+	if f0.String() != "123.456" {
+		t.Error("should be equal", f0.String(), "123.456")
+	}
+
+	f0 = NewF(1)
+	f1 = NewF(.5).Add(NewF(.5))
+	f2 := NewF(.3).Add(NewF(.3)).Add(NewF(.4))
+
+	if !f0.Equal(f1) {
+		t.Error("should be equal", f0, f1)
+	}
+	if !f0.Equal(f2) {
+		t.Error("should be equal", f0, f2)
+	}
+
+	f0 = NewF(.999)
+	if f0.String()!="0.999" {
+		t.Error("should be equal", f0,"0.999")
+	}
+}
+
+func TestMaxValue(t *testing.T) {
+	f0 := NewS("12345678901")
+	if f0.String()!="12345678901" {
+		t.Error("should be equal", f0, "12345678901")
+	}
+	f0 = NewS("123456789012")
+	if f0.String()!="NaN" {
+		t.Error("should be equal", f0, "NaN")
+	}
+	f0 = NewS("-12345678901")
+	if f0.String()!="-12345678901" {
+		t.Error("should be equal", f0, "-12345678901")
+	}
+	f0 = NewS("-123456789012")
+	if f0.String()!="NaN" {
+		t.Error("should be equal", f0, "NaN")
+	}
+	f0 = NewS("99999999999")
+	if f0.String()!="99999999999" {
+		t.Error("should be equal", f0, "99999999999")
+	}
+	f0 = NewS("9.9999999")
+	if f0.String()!="9.9999999" {
+		t.Error("should be equal", f0, "9.9999999")
+	}
+	f0 = NewS("99999999999.9999999")
+	if f0.String()!="99999999999.9999999" {
+		t.Error("should be equal", f0, "99999999999.9999999")
+	}
+
+}
+
+func TestFloat(t *testing.T) {
+	f0 := NewS("123.456")
+	f1 := NewF(123.456)
+
+	if !f0.Equal(f1) {
+		t.Error("should be equal", f0, f1)
+	}
+
+	f1 = NewF(0.0001)
+
+	if f1.String() != "0.0001" {
+		t.Error("should be equal", f1.String(), "0.0001")
+	}
+
+	f1 = NewS(".1")
+	f2 := NewS(NewF(f1.Float()).String())
+	if !f1.Equal(f2) {
+		t.Error("should be equal", f1, f2)
+	}
+
+}
+
+func TestInfinite(t *testing.T) {
+	f0 := NewS("0.10")
+	f1 := NewF(0.10)
+
+	if !f0.Equal(f1) {
+		t.Error("should be equal", f0, f1)
+	}
+
+	f2 := NewF(0.0)
+	for i := 0; i < 3; i++ {
+		f2 = f2.Add(NewF(.10))
+	}
+	if f2.String() != "0.3" {
+		t.Error("should be equal", f2.String(), "0.3")
+	}
+
+	f2 = NewF(0.0)
+	for i := 0; i < 10; i++ {
+		f2 = f2.Add(NewF(.10))
+	}
+	if f2.String() != "1" {
+		t.Error("should be equal", f2.String(), "1")
+	}
+
+}
+
+func TestAddSub(t *testing.T) {
+	f0 := NewS("1")
+	f1 := NewS("0.3333333")
+
+	f2 := f0.Sub(f1)
+	f2 = f2.Sub(f1)
+	f2 = f2.Sub(f1)
+
+	if f2.String() != "0.0000001" {
+		t.Error("should be equal", f2.String(), "0.0000001")
+	}
+	f2 = f2.Sub(NewS("0.0000001"))
+	if f2.String() != "0" {
+		t.Error("should be equal", f2.String(), "0")
+	}
+
+	f0 = NewS("0")
+	for i:=0;i<10;i++ {
+		f0 = f0.Add(NewS("0.1"))
+	}
+	if f0.String() != "1" {
+		t.Error("should be equal", f0.String(), "1")
+	}
+
+}
+
+func TestMulDiv(t *testing.T) {
+	f0 := NewS("123.456")
+	f1 := NewS("1000")
+
+	f2 := f0.Mul(f1)
+	if f2.String() != "123456" {
+		t.Error("should be equal", f2.String(), "123456")
+	}
+
+	f0 = NewS("2")
+	f1 = NewS("3")
+
+	f2 = f0.Div(f1)
+	if f2.String() != "0.6666666" {
+		t.Error("should be equal", f2.String(), "0.6666666")
+	}
+
+	f0 = NewS("1")
+	f1 = NewS("0.1")
+
+	f2 = f0.Mul(f1)
+	if f2.String() != "0.1" {
+		t.Error("should be equal", f2.String(), "0.1")
+	}
+
+
+}
+
+func TestNegatives(t *testing.T) {
+	f0 := NewS("99")
+	f1 := NewS("100")
+
+	f2 := f0.Sub(f1)
+	if f2.String() != "-1" {
+		t.Error("should be equal", f2.String(), "-1")
+	}
+	f0 = NewS("-1")
+	f1 = NewS("-1")
+
+	f2 = f0.Sub(f1)
+	if f2.String() != "0" {
+		t.Error("should be equal", f2.String(), "0")
+	}
+	f0 = NewS(".001")
+	f1 = NewS(".002")
+
+	f2 = f0.Sub(f1)
+	if f2.String() != "-0.001" {
+		t.Error("should be equal", f2.String(), "-0.001")
+	}
+}
+
+func TestOverflow(t *testing.T) {
+	f0 := NewF(1.1234567)
+	if f0.String() != "1.1234567" {
+		t.Error("should be equal", f0.String(), "1.1234567")
+	}
+	f0 = NewF(1.123456789123)
+	if f0.String() != "1.1234567" {
+		t.Error("should be equal", f0.String(), "1.1234567")
+	}
+	f0 = NewF(1.0 / 3.0)
+	if f0.String() != "0.3333333" {
+		t.Error("should be equal", f0.String(), "0.3333333")
+	}
+	f0 = NewF(2.0 / 3.0)
+	if f0.String() != "0.6666666" {
+		t.Error("should be equal", f0.String(), "0.6666666")
+	}
+
+}
+func TestNewRound(t *testing.T) {
+
+	f0 := NewRoundF(1.1234567)
+	if f0.String() != "1.1234567" {
+		t.Error("should be equal", f0.String(), "1.1234567")
+	}
+
+	f0 = NewRoundF(1.123456789123)
+	if f0.String() != "1.1234568" {
+		t.Error("should be equal", f0.String(), "1.1234568")
+	}
+
+	f0 = NewRoundF(1.0 / 3.0)
+	if f0.String() != "0.3333333" {
+		t.Error("should be equal", f0.String(), "0.3333333")
+	}
+
+	f0 = NewRoundF(2.0 / 3.0)
+
+	if f0.String() != "0.6666667" {
+		t.Error("should be equal", f0.String(), "0.6666667")
+	}
+}
+
+func TestNaN(t *testing.T) {
+	f0 := NewF(math.NaN())
+	if !f0.IsNaN() {
+		t.Error("f0 should be NaN")
+	}
+	if f0.String() != "NaN" {
+		t.Error("should be equal", f0.String(), "NaN")
+	}
+}
+
+func TestIntFrac(t *testing.T) {
+	f0 := NewF(1234.5678)
+	if f0.Int() != 1234 {
+		t.Error("should be equal", f0.Int(), 1234)
+	}
+	if f0.Frac() != .5678 {
+		t.Error("should be equal", f0.Frac(), .5678)
+	}
+}
+
+func TestString(t *testing.T) {
+	f0 := NewF(1234.5678)
+	if f0.String() != "1234.5678" {
+		t.Error("should be equal", f0.String(), "1234.5678")
+	}
+	f0 = NewF(1234.0)
+	if f0.String() != "1234" {
+		t.Error("should be equal", f0.String(), "1234")
+	}
+}
+
+
+func TestStringN(t *testing.T) {
+	f0 := NewS("1.1")
+	s := f0.StringN(2)
+
+	if s!="1.10" {
+		t.Error("should be equal", s, "1.10")
+	}
+	f0 = NewS("1.123")
+	s = f0.StringN(2)
+
+	if s!="1.12" {
+		t.Error("should be equal", s, "1.12")
+	}
+	f0 = NewS("1.123")
+	s = f0.StringN(2)
+
+	if s!="1.12" {
+		t.Error("should be equal", s, "1.12")
+	}
+	f0 = NewS("1.123")
+	s = f0.StringN(0)
+
+	if s!="1" {
+		t.Error("should be equal", s, "1")
+	}
+}
+
+func TestRound(t *testing.T){
+	f0 := NewS("1.12345")
+	f1 := f0.Round(2)
+
+	if f1.String()!="1.12" {
+		t.Error("should be equal", f1, "1.12")
+	}
+
+	f1 = f0.Round(5)
+
+	if f1.String()!="1.12345" {
+		t.Error("should be equal", f1, "1.12345")
+	}
+	f1 = f0.Round(4)
+
+	if f1.String()!="1.1235" {
+		t.Error("should be equal", f1, "1.1235")
+	}
+}
